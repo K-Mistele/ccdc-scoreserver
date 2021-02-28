97286f694a7055fcbcfac77be14293a4e37131e7
Kyle Mistele <kyle@mistele.com>
2021-02-27 21:10:25 -0600

    Add time zone as a constant

--- lib/constants/constants.go
+++ lib/constants/constants.go
@@ -1,4 +1,7 @@
 package constants
 
+import "time"
+
 const UserAgent = "Blacklight-ScoreServer-1.0"
 const BrowserFriendlyUserAgent = "Blacklight-ScoreServer 1.0, like: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
+var ServerTime, _ = time.LoadLocation("America/Chicago")
