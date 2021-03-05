5d94b8372d35bb93b5b6746d665abb00a5c320ee
Kyle Mistele <kyle@mistele.com>
2021-03-04 20:57:30 -0600

    Update connection informatioN

--- lib/database/constants.go
+++ lib/database/constants.go
@@ -1,7 +1,7 @@
 package database
 
 // TODO CHANGE THIS WHEN WE'RE BACK IN DOCKER
-const URI = "mongodb://localhost:27017/"
+const URI = "mongodb://ccdc-scoreserver-database:27017/"
 const Database = "ccdc-scoreserver"
 
 type Collection string
